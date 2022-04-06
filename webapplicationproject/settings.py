51bfb078dd2f414e642ffca22745ceca4c098286
Sammy Issa <sammissa@amazon.com>
2022-04-06 16:35:03 +0100

    change settings

--- webapplicationproject/settings.py
+++ webapplicationproject/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'application.apps.ApplicationConfig',
-    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
